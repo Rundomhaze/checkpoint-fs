d3bfa57a0a5a8981ad12e9bf4ecfc672c4e0b60e
Alex <petenkov2822@gmail.com>
2022-05-17 20:27:25 +0400

    1 test

--- padawans.js
+++ padawans.js
@@ -1,4 +1,23 @@
+const fs = require('fs');
+function getPadawanNames() {
+  let padawans = fs.readFileSync('data/padawans.txt', 'utf-8')
+  padawans = padawans.split('\n')
+  padawans.pop()
+  return padawans
+}
 
+function getLightsaberScores() {
+  let scores = fs.readFileSync('data/scores.txt', 'utf-8')
+  return scores.split(' ').slice(0, scores.length - 1);
+}
+
+function getStats() {
+  
+}
+
+function writeStats() {
+  
+}
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
